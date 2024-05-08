1b812f6a0d5d6d8a047ab952d782bf36855a88f2
masoudyousefi1999 <masoudyousefi1999@gmail.com>
2024-05-08 18:33:18 +0330

    change view setting

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const { v4: uudiv4 } = require("uuid");
 require("dotenv").config()
 
 app.set("view engine", "ejs");
+app.set('views', 'views')
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
